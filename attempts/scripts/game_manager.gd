06929d0d417af6d4d8086625c3212810ae80c500
Juan Keny <89154693+lorde-ives@users.noreply.github.com>
2025-04-11 22:28:41 +0300

    Add files via upload
    Godot project files

---
+++ attempts/scripts/game_manager.gd
@@ -0,0 +1,9 @@
+extends Node
+
+var score = 0
+@onready var score_label = $score_label
+
+func add_point():
+	score += 1
+	print(score)
+	score_label.text = "You collected " + str(score) + " coins."
