8e08c703762b9b3c477618c8dc3168c57ffc1815
Juan Keny <89154693+lorde-ives@users.noreply.github.com>
2025-04-12 16:44:50 +0300

    Add files via upload
    Bigger map, more enemies and switch from Godot 4.2 to 4.4

--- attempts/scripts/slime.gd
+++ attempts/scripts/slime.gd
@@ -6,17 +6,26 @@ var direction = 1
 @onready var ray_cast_right = $RayCastRight
 @onready var ray_cast_left = $RayCastLeft
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var ray_cast_down: RayCast2D = $RayCastDown
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if ray_cast_right.is_colliding():
-		direction = -1
-		
-		#FLIPPING THE SPRITE
-		animated_sprite_2d.flip_h = true
-	if ray_cast_left.is_colliding():
-		direction = 1
-		animated_sprite_2d.flip_h = false
+	if ray_cast_down.is_colliding():
+		if ray_cast_right.is_colliding():
+			direction = -1
+			
+			#FLIPPING THE SPRITE
+			animated_sprite_2d.flip_h = true
+		if ray_cast_left.is_colliding():
+			direction = 1
+			animated_sprite_2d.flip_h = false
+	else:
+		if direction == 1:
+			direction = -1
+			#FLIPPING THE SPRITE
+			animated_sprite_2d.flip_h = true
+		elif direction == -1:
+			direction = 1
+			animated_sprite_2d.flip_h = false
 	position.x += direction * SPEED * delta
-	
 	
